8d8de3104d9d3db90db511f5e78e21e754495dde
Aurelien Moreau <aurelien.moreau@yienyien.net>
2015-12-28 12:57:35 +0100

    Fix dependency bug: angus service framework name is angus-framework.

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 
 from setuptools import setup, find_packages
 
-__updated__ = "2015-05-29"
+__updated__ = "2015-12-28"
 __author__ = "Aurélien Moreau"
 __copyright__ = "Copyright 2015, Angus.ai"
 __credits__ = ["Aurélien Moreau", "Gwennaël Gâté"]
 __status__ = "Production"
 
-setup(name='angus-api-dummy',
+setup(name='angus-service-dummy',
       version="0.0.1",
       description='Angus Dummy Service',
       author=__author__,
       author_email='aurelien.moreau@angus.ai',
       url='http://www.angus.ai/',
       install_requires=[
-          "pytest==2.6.4",
-          "angus-api-framework",
+          "angus-framework",
       ],
       packages=find_packages(),
       )
